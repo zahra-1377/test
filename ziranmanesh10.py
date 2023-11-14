7707db983009b51cb5210ed3908098c11153b011
Zahra <ziranmanesh1@toromail.csudh.edu>
2023-11-14 07:02:24 +0000

    First version python program

---
+++ ziranmanesh10.py
@@ -0,0 +1,8 @@
+#Zahra Iranmanesh
+#LAB 10
+
+number = int(input("Please enter the number: "))
+square = number ** 2
+
+print(f"The result is {square}.")
+print(" The early bird catches the worn. ")
